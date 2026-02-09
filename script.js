5638dffa3b12d14af95691847e4c0e9fd9e536f8
johnhmoura-hash <john.h.moura@aluno.senai.br>
2026-02-08 22:43:00 -0300

    atualizações
    adicionei interatividade no slider

--- script.js
+++ script.js
@@ -10,8 +10,41 @@ function validarFormulario() {
 
 /* slider */
 
-var imgSlider = document.querySelectorAll('.slider-container .slider-box');
-var btnNav = document.querySelectorAll('.btn-nav-box .btn-nav');
+let imgSlider = document.querySelectorAll('.slider-container .slider-box');
+let btnNav = document.querySelectorAll('.btn-nav-box .btn-nav');
 
 let contadorImg = imgSlider.length;
 let imgAtiva = 0;
+
+function mostrarSlider(){
+
+    let antigaImg = document.querySelector('.slider-container .slider-box.ativo')
+    let antigoBtnNav = document.querySelector('.btn-nav-box .btn-nav.ativo');
+
+    antigaImg.classList.remove('ativo')
+    antigoBtnNav.classList.remove('ativo');
+
+    imgSlider[imgAtiva].classList.add('ativo');
+    btnNav[imgAtiva].classList.add('ativo');
+}
+
+function iniciarAutoPlay(){
+    intervalo = setInterval(() => {
+        imgAtiva++;
+
+        if(imgAtiva >= contadorImg){
+            imgAtiva = 0;
+        }
+
+        mostrarSlider();
+    }, 2500);
+}
+
+btnNav.forEach((btn, indice)=>{
+    btn.addEventListener('click', ()=>{
+        imgAtiva = indice;
+        mostrarSlider();
+    });
+});
+
+iniciarAutoPlay();
